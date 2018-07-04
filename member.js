3ab76988352315eeda1b41fbe3c32e782ecc1621
Richard Rodger <richard@ricebridge.com>
2018-07-04 18:40:08 +0100

    children

--- member.js
+++ member.js
@@ -14,6 +14,7 @@ const W = Util.promisify
 
 
 const optioner = Optioner({
+  kinds:{}
 })
 
 var error = (exports.error = Eraro({
@@ -27,17 +28,19 @@ module.exports = function member(options) {
   const opts = optioner.check(options)
 
   seneca
-    .add('role:member,cmd:add', cmd_add)
+    .add('role:member,add:member', add_member)
+    .add('role:member,list:children', list_children)
+    .add('role:member,list:parents', list_parents)
 
   
-  function cmd_add(msg, reply) {
+  function add_member(msg, reply) {
     const seneca = this
     work().then(reply).catch(reply)
   
     async function work() {
       const member_ent = WE(seneca.make('sys/member'))
 
-      const prev = await member_ent.make$().load$({
+      const prev = await WE(member_ent.make$()).load$({
         p: msg.parent,
         c: msg.child,
         k: msg.kind,
@@ -52,19 +55,84 @@ module.exports = function member(options) {
         }).save$()
       }
       else {
-        member = await WE(member_ent.make$()).data$({
+        const data = {
           p: msg.parent,
           c: msg.child,
           k: msg.kind,
           d: msg.code,
           t: msg.tags
-        }).save$()
+        }
+        if(msg.id) {
+          data.id$ = msg.id
+        }
+        member = await WE(member_ent.make$()).data$(data).save$()
       }
 
       return member
     }
   }
   
+  function list_children(msg, reply) {
+    const seneca = this
+    work().then(reply).catch(reply)
+  
+    async function work() {
+      const member_ent = WE(seneca.make('sys/member'))
+
+      const fields = []
+      const q = {}
+
+      if(null != msg.parent) {
+        q.p = msg.parent
+      }
+      if(null != msg.kind) {
+        q.k = msg.kind
+      }
+      if(null != msg.code) {
+        q.d = msg.code
+      }
+      
+      var list = await member_ent.list$(q)
+      //console.log('LIST',q,list)
+      
+      var seen = {}
+      list = list.filter(x => seen[x.c] ? false : seen[x.c] = true)
+
+      if(null == msg.as || 'child-id' == msg.as) {
+        list = list.map(x=>x.c)
+      }
+      if('member-id' == msg.as) {
+        list = list.map(x=>x.id)
+      }
+      else if('member' == msg.as) {
+        // use list as is
+      }
+      else if('child' == msg.as) {
+        list = await load_items(seneca,list,'c')
+      }
+      
+      return {items:list}
+    }    
+  }
+
+  function list_parents(msg, reply) {
+
+  }
+
+  async function load_items(seneca, list, type) {
+    const out = []
+
+    for(var i = 0; i < list.length; i++) {
+      var kind = list[i].k
+      var canon = opts.kinds[kind][type]
+      var ent = WE(seneca.make(canon))
+      var entid = list[i][type]
+
+      out.push( await ent.load$(entid) )
+    }
+
+    return out
+  }
   
   return {
     export: {
@@ -77,10 +145,10 @@ const intern = (module.exports.intern = {
 
 
 function WE(ent) {
-  const make = ent.make$
-  ent.make$ = function() {
-    return WE(make.apply(ent,arguments))
-  }
+  //const make = ent.make$
+  //ent.make$ = function() {
+  //  return WE(make.apply(ent,arguments))
+  //}
   ent.load$ = W(ent.load$.bind(ent))
   ent.save$ = W(ent.save$.bind(ent))
   ent.list$ = W(ent.list$.bind(ent))
--- test/member.test.js
+++ test/member.test.js
@@ -12,11 +12,15 @@ const PluginValidator = require('seneca-plugin-validator')
 const Seneca = require('seneca')
 const Plugin = require('..')
 
+
+const W = Util.promisify
+
+
 lab.test('validate', PluginValidator(Plugin, module))
 
 lab.test('happy', fin => {
   make_instance(fin)
-    .act('role:member,cmd:add',
+    .act('role:member,add:member',
          {parent:'p0', child:'c0', kind:'group', code:'admin', tags:['foo','bar']},
          function(err, out) {
            expect(out).exist()
@@ -27,7 +31,7 @@ lab.test('happy', fin => {
 
 lab.test('no-dups', fin => {
   make_instance(fin)
-    .act('role:member,cmd:add',
+    .act('role:member,add:member',
          {parent:'p0', child:'c0', kind:'group', code:'admin', tags:['foo','bar']},
          function(err, out) {
            expect(out).exist()
@@ -36,7 +40,7 @@ lab.test('no-dups', fin => {
 
            this
              .act(
-               'role:member,cmd:add',
+               'role:member,add:member',
                {parent:'p0', child:'c0',
                 kind:'group', code:'admin', tags:['zed']},
                function(err, out) {
@@ -51,10 +55,98 @@ lab.test('no-dups', fin => {
 })
 
 
+lab.test('list-children', fin => {
+  make_data0(make_instance(fin), function(si) {
+    si
+      .gate()
+
+      .act('role:member,list:children', function(err, out) {
+        expect(out.items).equal([ 'c0', 'c1', 'c2', 'c3', 'c4' ])
+      })
+      .act('role:member,list:children,parent:p0', function(err, out) {
+        expect(out.items).equal([ 'c0', 'c1', 'c2', 'c3' ])
+      })
+      .act('role:member,list:children,parent:p0,kind:k0', function(err, out) {
+        expect(out.items).equal([ 'c0', 'c1', 'c2' ])
+      })
+      .act('role:member,list:children,parent:p0,kind:k0,code:d0', function(err, out) {
+        expect(out.items).equal([ 'c0', 'c1' ])
+      })
+
+      .act('role:member,list:children,parent:p0,kind:k0,code:d0,as:child-id', function(err, out) {
+        expect(out.items).equal([ 'c0', 'c1' ])
+      })
+      .act('role:member,list:children,parent:p0,kind:k0,code:d0,as:member-id', function(err, out) {
+        expect(out.items).equal([ 'm0', 'm1' ])
+      })
+      .act('role:member,list:children,parent:p0,kind:k0,code:d0,as:member', function(err, out) {
+        expect(out.items.map(x=>x.id)).equal([ 'm0', 'm1' ])
+      })
+
+      .act('role:member,list:children,parent:p0,kind:k0,code:d0,as:child', function(err, out) {
+        //console.log(out)
+        expect(out.items[0].toString()).equal('$-/-/bar;id=c0;{f2:100,f3:A}')
+        expect(out.items[1].toString()).equal('$-/-/bar;id=c1;{f2:101,f3:B}')
+      })
+
+      .ready(fin)
+  })
+})
+
+
 lab.test('intern', fin => {
   expect(Plugin.intern).exists()
   fin()
 })
+
+
+function make_data0(si, done) {
+  const act = W(si.act.bind(si))
+  const foo_ent = WE(si.make('foo'))
+  const bar_ent = WE(si.make('bar'))
+  const zed_ent = WE(si.make('zed'))
+  
+  work().then(done).catch(done)
+
+  async function work() {
+    await foo_ent.make$().data$({id$:'p0',f0:0,f1:'a'}).save$()
+    await foo_ent.make$().data$({id$:'p1',f0:1,f1:'b'}).save$()
+
+    await bar_ent.make$().data$({id$:'c0',f2:100,f3:'A'}).save$()
+    await bar_ent.make$().data$({id$:'c1',f2:101,f3:'B'}).save$()
+    await bar_ent.make$().data$({id$:'c2',f2:102,f3:'C'}).save$()
+    await bar_ent.make$().data$({id$:'c3',f2:103,f3:'D'}).save$()
+
+    await zed_ent.make$().data$({id$:'c4',f4:1000,f5:'aa'}).save$()
+    
+    const m0 = await act(
+      'role:member,add:member',
+      {id:'m0', parent:'p0', child:'c0', kind:'k0', code:'d0', tags:['t0']})
+
+    const m1 = await act(
+      'role:member,add:member',
+      {id:'m1', parent:'p0', child:'c1', kind:'k0', code:'d0', tags:['t0','t1']})
+
+    const m2 = await act(
+      'role:member,add:member',
+      {id:'m2', parent:'p0', child:'c2', kind:'k0', code:'d1', tags:['t1','t2']})
+
+    const m3 = await act(
+      'role:member,add:member',
+      {id:'m3', parent:'p0', child:'c3', kind:'k1', code:'d2', tags:['t0']})
+
+    const m4 = await act(
+      'role:member,add:member',
+      {id:'m4', parent:'p1', child:'c0', kind:'k0', code:'d0', tags:['t0']})
+
+    const m5 = await act(
+      'role:member,add:member',
+      {id:'m5', parent:'p1', child:'c4', kind:'k0', code:'d0', tags:['t1']})
+
+    return si
+  }
+}
+
 
 
 function make_instance(fin) {
@@ -62,5 +154,24 @@ function make_instance(fin) {
     .test(fin)
     .use('entity')
     .use(Plugin, {
+      kinds: {
+        k0: {
+          p: 'foo',
+          c: 'bar'
+        },
+        k1: {
+          p: 'foo',
+          c: 'zed'
+        }
+      }
     })
+}
+
+
+function WE(ent) {
+  ent.load$ = W(ent.load$.bind(ent))
+  ent.save$ = W(ent.save$.bind(ent))
+  ent.list$ = W(ent.list$.bind(ent))
+  ent.remove$ = W(ent.remove$.bind(ent))
+  return ent
 }
